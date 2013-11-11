a23226454ca98157a8610d1bed3c6c36f3f6c074
svallero <svallero@to.infn.it>
2013-11-11 17:48:18 +0100

    Debugged for production + http proxy

---
+++ CloudInitFiles/preconfig.py
@@ -0,0 +1,32 @@
+#cloud-boothook
+#!/usr/bin/python
+# vim: syntax=python
+
+# Contact: svallero@to.infn.it
+ 
+import os
+import urllib2
+from subprocess import Popen,PIPE
+
+# Define logfile
+logname = '/var/log/cloud-init-preconfig.log'
+# Import script with definition of logger and some useful function 
+# to avoid duplicating the same code on all modules
+response = urllib2.urlopen('http://srm-dom0.to.infn.it/test/header.py')
+exec (response.read())
+
+# Set proxy for yum
+proxy='https://proxy2.to.infn.it:3128'
+filename = '/etc/yum.conf' 
+file = open(filename) 
+if 'proxy' not in file:
+  logger.info('adding proxy to yum configuration...')
+  try:
+    # write yum.conf
+    cmd = ('echo "proxy: '+proxy+'" >> '+filename+'')
+    DPopen(cmd,'True')
+    # global setting
+    cmd = ('export https_proxy='+proxy+'')
+    DPopen(cmd,'True')
+  except:
+    logger.error('could not patch '+filename+'!')
--- CloudInitFiles/summary_log.py
+++ CloudInitFiles/summary_log.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
+
+# Contact: svallero@to.infn.it
+
 import os
 import urllib2
 
 # Write below list of strings to be ignored
-exceptions = ['The munge key file /etc/munge/munge.key is not found']
+# Capire come mai e cercare di eliminare gli errori TODO
+exceptions = ['The munge key file /etc/munge/munge.key is not found','install ig-vomscerts-all\' returned 1', 'File[zabbix_agentd_conf]/ensure: change from absent to file failed', 'install lcg-vomscerts-desy\' returned 1']
 
 logname = '/var/log/cloud-summary.log'
 response = urllib2.urlopen('http://srm-dom0.to.infn.it/test/header.py')
@@ -17,16 +21,21 @@ for file in files.splitlines():
    if (os.stat(file).st_size != 0):
      fh = open(file)
      for line in fh:
-       if 'ERROR' in line:
+       if 'ERROR' in line or 'merr' in line:
+         # 'merr' comes from puppet, for some reason the exception 
+         # is not caught by python 
          if not any(e in line for e in exceptions):
            logger.error(file)
            errors  = True
            break 
-         else:
-           # in case of the munge.key error I want to skip also the following
-           # line, but need to check if it's ok also in case of other exceptions
+         elif 'munge' in line:
+           # in case of the munge.key error I want to skip also 
+           # the following line
            line = fh.next()       
            
      if not errors:
-       logger.log(14,file)   
+       logger.log(14,file)  
+outf=open(logname)
+outf.write('In case of errors in "cloud-init-puppetconfig.log" also grep for "merr"!') 
+outf.close()
 print ('Summary written in '+logname+'')
